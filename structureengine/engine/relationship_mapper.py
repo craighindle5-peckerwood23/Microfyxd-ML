4c45fcda3b58d898e6b5bba0fcd2ab180a8cb202
Craig Hindle JR <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-07-13 17:10:02 -0700

    Delete structureengine directory

--- structureengine/engine/relationship_mapper.py
+++
@@ -1,3 +0,0 @@
-class RelationshipMapper:
-    def map(self, entities):
-        return {"relationships": [{"entity": e["text"], "type": e["label"]} for e in entities]}
--- structureengine/intentclassifier.py
+++
@@ -1,7 +0,0 @@
-class IntentClassifier:
-    def classify(self, text):
-        if "refund" in text.lower():
-            return "refund_request"
-        if "schedule" in text.lower():
-            return "scheduling"
-        return "general"
