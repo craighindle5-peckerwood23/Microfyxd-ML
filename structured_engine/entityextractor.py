4be0bb23b77c98d9196b78c5ace1805c8b7789e6
Craig Hindle JR <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-07-13 17:10:18 -0700

    Delete structured_engine directory

--- structured_engine/entityextractor.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
-
-class EntityExtractor:
-    def extract(self, text):
-        doc = nlp(text)
-        return [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
